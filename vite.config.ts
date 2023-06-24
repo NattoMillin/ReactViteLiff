c77b8b64fb621495cc2b1e0f0e79dd63939fd6f7
NattoMillin <zz112ggww@icloud.com>
2023-06-25 00:19:42 +0900

    first commit & build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: "/",
+  base: "/",
+  publicDir: "public",
   plugins: [react()],
-})
+  build: {
+    // `root` からの相対パスで指定する
+    outDir: "/dist",
+  },
+  envDir: "/",
+});
