3f764df3dfc961dc7d55490e972c193112d5ff00
NattoMillin <zz112ggww@icloud.com>
2023-06-25 01:16:47 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
-import { Suspense } from "react";
-import { Loading } from "./components/Loading";
+// import { Suspense } from "react";
+// import { Loading } from "./components/Loading";
 function App() {
-  return (
-    <BrowserRouter>
-      <Suspense fallback={<Loading />}>
-        <Routes>
-          <Route path="./" element={<Home />} />
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
-  );
+  return <Home />;
 }
 export default App;
+/*
+<BrowserRouter>
+<Suspense fallback={<Loading />}>
+  <Routes>
+    <Route path="/" element={<Home />} />
+  </Routes>
+</Suspense>
+</BrowserRouter>
+*/
