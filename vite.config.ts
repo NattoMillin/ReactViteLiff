fdcd8b60b7714357c6fcbe9ada5b69580a2f3ce4
NattoMillin <zz112ggww@icloud.com>
2023-06-25 00:47:56 +0900

    まじで相対パスの設定ミススンナ

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: "/",
-  base: "/",
+  root: "./",
+  base: "./",
   publicDir: "public",
   plugins: [react()],
   build: {
     // `root` からの相対パスで指定する
-    outDir: "/dist",
+    outDir: "./dist",
   },
-  envDir: "/",
+  envDir: "./",
 });
