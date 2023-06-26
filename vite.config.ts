7e9ff7c862f4ee3cba0d78f9262f62327aba0f04
Natto Millin <zz112ggww@icloud.com>
2023-06-26 01:51:21 +0000

    please

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "./",
-  base: "/ReactViteLiff/",
-  //base: "/",
+  base: "/",
   publicDir: "public",
   plugins: [react()],
   build: {
