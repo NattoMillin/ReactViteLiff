53dacff2b95482a0b886dd9a89919acc64e4e355
NattoMillin <zz112ggww@icloud.com>
2023-06-29 23:37:02 +0900

    Create Type

--- src/learningComponents/Validate/Validate.ts
+++ src/learningComponents/Validate/Validate.ts
@@ -1,22 +1,33 @@
 import * as yup from "yup";
 
-const onNewlineValidator = (errorMessage: string) =>
-  yup
-    .string()
-    .test("no-newline", errorMessage, (value) => !value?.includes("@"))
-    .required("このフィールドは必須です。");
+// const onNewlineValidator = (errorMessage: string) =>
+//   yup
+//     .string()
+//     .test("no-newline", errorMessage, (value) => !value?.includes("@"))
+//     .required("このフィールドは必須です。");
 
 export const schema = yup.object({
-  Employment_Classification: yup.string().required("必須です"),
-  employment: yup.string().required("必須です"),
-  welfare_programme_Check: yup.string(),
-  welfare_programme_Text: yup.string().required("必須です"),
-  heat_health: yup.string(),
-  heat_health_Radio: yup.string(),
-  heat_health_Text: yup.string(),
-  hete: yup.string(),
-  hete_Radio: yup.string(),
-  hete_Text: yup.string(),
+  Employment_Classification: yup.string().required(),
+  employment: yup.string().required(),
+  welfare_programme_Check: yup.string().required(),
+  welfare_programme_Text: yup.string().required(),
+  heat_health: yup.string().required(),
+  heat_health_Radio: yup.string().required(),
+  heat_health_Text: yup.string().required(),
+  hete: yup.string().required(),
+  hete_Radio: yup.string().required(),
+  hete_Text: yup.string().required(),
 });
 
-export type FormType = yup.InferType<typeof schema>;
+export type FormType = {
+  Employment_Classification: string;
+  employment: string;
+  welfare_programme_Check: string;
+  welfare_programme_Text: string;
+  heat_health: string;
+  heat_health_Radio: string;
+  heat_health_Text: string;
+  hete: string;
+  hete_Radio: string;
+  hete_Text: string;
+};
